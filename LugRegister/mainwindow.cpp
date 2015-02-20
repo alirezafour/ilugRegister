450cfa55c264344fcae53b1761389bbee8e5c8d4
Alireza Hor <alirezafour@gmail.com>
2015-02-20 14:29:39 +0330

    add dafault set and title and date config to the class mainwindow.cpp

--- LugRegister/mainwindow.cpp
+++ LugRegister/mainwindow.cpp
@@ -2,11 +2,23 @@
 #include "ui_mainwindow.h"
 #include "database_export.h"
 
+
+
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    //default set
+    ui->Date_Line->setText("yyyy-mm-dd");
+
+    //  Title & Date Config
+    setWindowTitle("Lug Register");
+    curentDate = QDate::currentDate();
+    curentDate_Str = curentDate.toString(Qt::ISODate);
+    ui->todayDay_lbl->setText(curentDate_Str);
+    ui->todayDay_lbl_SelectTab->setText(curentDate_Str);
 }
 
 MainWindow::~MainWindow()
