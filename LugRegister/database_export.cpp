c25eed0b83844beb806e1d7926033b1fd1711779
Alireza Hor <alirezafour@gmail.com>
2015-02-20 14:21:26 +0330

    add openFile Function to database_export.cpp

--- LugRegister/database_export.cpp
+++ LugRegister/database_export.cpp
@@ -5,3 +5,14 @@ database_Export::database_Export()
 }
 
 
+bool database_Export::openFile(QString location)
+{
+    file.setFileName(location);
+    if(file.open(QIODevice::ReadWrite))
+    {
+        file.setPermissions(QFile::ReadOwner|QFile::WriteOwner|QFile::ExeOwner|QFile::ReadGroup|QFile::WriteGroup|QFile::ExeGroup|QFile::ReadOther|QFile::WriteOther|QFile::ExeOther);
+        return true;
+    }
+    else
+        return false;
+}
